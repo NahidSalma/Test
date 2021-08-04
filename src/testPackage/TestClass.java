e7cf8d1db57da82cb14871b73abbd9200869f4a0
“Salma” <h.n222@yahoo.com>
2021-08-03 23:40:56 -0400

    1st comments

---
+++ src/testPackage/TestClass.java
@@ -0,0 +1,11 @@
+package testPackage;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("HELLO WORLD");
+        System.out.println("HELLO WORLD");
+
+    }
+}
